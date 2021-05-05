639a2f9a255b9771dfe8f7debdbc206b90de2e41
dojo24 <melissalongenberger24@gmail.com>
2021-05-05 16:58:28 -0400

    small updates

--- crochetApp/admin.py
+++ crochetApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Products, User, OwnerUser
+from .models import User, OwnerUser, Category, Products
 
-admin.site.register(Products)
 admin.site.register(User)
 admin.site.register(OwnerUser)
+admin.site.register(Category)
+admin.site.register(Products)
